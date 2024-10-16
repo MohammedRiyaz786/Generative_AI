69a3372f27801db10c2c5ae520a54dfd1269c1be
MohammedRiyaz786 <mohammed.riyaz@testingxperts.com>
2024-10-16 12:23:11 +0530

    debug

--- Chat_With_Pdf/Local_Model/updated_app.py
+++ Chat_With_Pdf/Local_Model/updated_app.py
@@ -462,6 +462,7 @@ def main():
                 st.warning("Please upload files before processing.")
 
     user_question = st.text_input("Ask a Question from the Uploaded Files")
+    print(user_question)
     if user_question:
         handle_user_input(user_question)
 
